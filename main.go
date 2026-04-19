16443189efbdb9417b9806e1b7465bf6a45359b3
pphui8 <jh1216595344@gmail.com>
2026-04-19 16:17:32 +0900

    update

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -12,5 +13,5 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":9000")
 }
