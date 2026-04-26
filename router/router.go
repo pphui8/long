a37d701c8d5d0933f6d082582afd7accbc87a3f4
pphui8 <jh1216595344@gmail.com>
2026-04-26 23:55:51 +0900

    update

--- router/router.go
+++ router/router.go
@@ -9,6 +9,9 @@ import (
 func Setup() *gin.Engine {
 	r := gin.Default()
 
+	// CORS middleware
+	r.Use(CORSMiddleware())
+
 	// Public routes
 	r.POST("/login", handler.HandleLogin)
 	r.POST("/refresh", handler.HandleRefresh)
@@ -23,4 +26,20 @@ func Setup() *gin.Engine {
 	}
 
 	return r
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
